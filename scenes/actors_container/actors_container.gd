ca45f0c4b92a1d3b3e2e3163893447e6c9197c22
hanmi255 <hanmi2550505@gmail.com>
2025-07-13 09:20:46 +0800

    feat(data): 重构数据加载逻辑并更新场景配置
    - 将 LoadData 类重命名为 DataLoader，优化数据加载功能
    - 新增 data_loader.gd 文件，负责从 JSON 文件加载球队和球员数据
    - 更新 actors_container.gd，调整玩家生成逻辑，使用新的数据加载类
    - 修改 actors_container.tscn，重命名 Players 节点为 PlayersContainer，优化场景结构
    - 更新球员生成函数，确保正确传递参数并添加测试球员

--- scenes/actors_container/actors_container.gd
+++ scenes/actors_container/actors_container.gd
@@ -10,6 +10,7 @@ const PLAYER_SCENE := preload("res://scenes/player/player.tscn")
 @export var team_away: String
 
 @onready var spawns: Node2D = $Spawns
+@onready var players_container: Node2D = $PlayersContainer
 
 
 func _ready() -> void:
@@ -17,19 +18,24 @@ func _ready() -> void:
 	spawns.scale.x = -1
 	spawn_players(team_away, goal_away)
 
+	# 添加测试球员
+	var player_test: Player = players_container.get_child(4)
+	player_test.control_scheme = Player.ControlScheme.P1
+	player_test._set_control_scheme_sprite()
+	
 
 func spawn_players(team_name: String, own_goal: Goal) -> void:
-	var players := LoadData.get_squad(team_name)
+	var players := DataLoader.get_squad(team_name)
 	var target_goal := goal_away if own_goal == goal_home else goal_home
 
 	for i in players.size():
 		var player_pos := spawns.get_child(i).global_position as Vector2
 		var player_data := players[i] as PlayerResource
-		var player := spawn_player(player_pos, ball, own_goal, target_goal, player_data)
-		add_child(player)
+		var player := spawn_player(player_pos, own_goal, target_goal, player_data)
+		players_container.add_child(player)
 
 
-func spawn_player(player_pos: Vector2, ball_ref: Ball, own_goal: Goal, target_goal: Goal, player_data: PlayerResource) -> Player:
+func spawn_player(player_pos: Vector2, own_goal: Goal, target_goal: Goal, player_data: PlayerResource) -> Player:
 	var player := PLAYER_SCENE.instantiate()
-	player.initialize(player_pos, ball_ref, own_goal, target_goal, player_data)
+	player.initialize(player_pos, ball, own_goal, target_goal, player_data)
 	return player
